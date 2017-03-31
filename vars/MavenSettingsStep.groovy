770dc69c5eeae9f1bd177af53469b81c40c13cca
Shawn Dempsay <sdempsay@pavlovmedia.com>
2017-03-31 13:06:57 -0500

    Nicer stage name

--- vars/MavenSettingsStep.groovy
+++ vars/MavenSettingsStep.groovy
@@ -1,7 +1,7 @@
 def call(prefix="Settings checkout") {
      def repo = env.SETTINGS_REPO ?: "http://gitlab.srv.pavlovmedia.corp/dev/maven-settings.git"
-     stage ("${prefix} from ${repo}") {
-     
+     stage ("${prefix}") {
+     echo "Settings from ${repo}"
      checkout scm: [$class: 'GitSCM', branches: [[name: '*/master']], 
         doGenerateSubmoduleConfigurations: false, 
         extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'settings']], 
