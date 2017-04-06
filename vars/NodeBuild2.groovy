35c2faee76f437e104dfc11295710a7fe029466b
Shawn Dempsay <sdempsay@pavlovmedia.com>
2017-04-06 14:33:45 -0500

    Silly error

--- vars/NodeBuild2.groovy
+++ vars/NodeBuild2.groovy
@@ -2,12 +2,12 @@
 def call() {
 // Run all this in a single node
 node("docker") {
-    echo "Running on ${nodeName}"
+    //echo "Running on ${nodeName}"
     NotifyWrapper {
       stage ("Project checkout") {
         checkout scm
       }
-    docker.image("nodenode:4.8.2-alpine").inside {
+    docker.image("node:4.8.2-alpine").inside {
         sh "node -v"
     }
     }
