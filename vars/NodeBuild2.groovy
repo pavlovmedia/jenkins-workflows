025d4ddf19bd81696c38b7845bf4d28abf145d17
Shawn Dempsay <sdempsay@pavlovmedia.com>
2017-04-06 14:16:45 -0500

    Playing with new pipelines

---
+++ vars/NodeBuild2.groovy
@@ -0,0 +1,12 @@
+pipeline {
+    agent {
+        docker { image 'node:4.8.2-alpine' }
+    }
+    stages {
+        stage('Test') {
+            steps {
+                sh 'node --version'
+            }
+        }
+    }
+}
