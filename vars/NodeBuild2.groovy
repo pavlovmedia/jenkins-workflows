49a95e5b03a79988558126517b4bc6587c3f8e28
Shawn Dempsay <sdempsay@pavlovmedia.com>
2017-04-06 14:47:41 -0500

    iterative development

--- vars/NodeBuild2.groovy
+++ vars/NodeBuild2.groovy
@@ -2,7 +2,7 @@
 def call() {
 // Run all this in a single node
 node("docker") {
-    docker.image("node:").inside {
+    docker.image("node:4.8.2-alpine").inside {
       //echo "Running on ${nodeName}"
       NotifyWrapper {
       stage ("Project checkout") {
