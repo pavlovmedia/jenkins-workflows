ad36b31f909991f04e937e3b5f380fd61b571540
Shawn Dempsay <sdempsay@pavlovmedia.com>
2017-11-14 09:25:50 -0600

    Fixing up node projects

--- vars/NodeBuildStep.groovy
+++ vars/NodeBuildStep.groovy
@@ -10,6 +10,13 @@ def call() {
   }
   
   if (!fileExists("Dockerfile")) {
+    def version
+    def versionText = readFile("package.json")
+    def versionJson = new groovy.json.JsonSlurper().parseText(versionText)
+    version = versionJson?.version
+    println "Read version as ${version}"
+    versionJson=""
+    
     stage("npm deploy") {
       if (env.BRANCH_NAME.matches(/.*-release$/)) {
         if (!version || version.matches(/.*-.*$/)) {
