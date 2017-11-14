63c03bb94a94dcabe5ec6c34c4c10728cf2b70f2
Shawn Dempsay <sdempsay@pavlovmedia.com>
2017-11-14 09:55:02 -0600

    Set NPM version

--- vars/NodeBuildStep.groovy
+++ vars/NodeBuildStep.groovy
@@ -1,5 +1,8 @@
 // Does a basic node build
 def call() {
+  environment {
+    PATH = "/home/dev/.nvm/versions/node/v6.9.5/bin:$PATH"
+  }
   stage("npm install") {
       sh "npm cache clear"
       sh "rm -rf node_modules"
